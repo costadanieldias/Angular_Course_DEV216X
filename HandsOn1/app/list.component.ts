3d64330142583cf5c9469a3e5f4213e0fa531add
Daniel Dias Costa <Daniel Dias@prodespluz.local>
2018-08-31 16:06:15 -0300

    First commit

---
+++ HandsOn1/app/list.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'list-component',
+  templateUrl: 'app/views/list.component.html'
+})
+export class ListComponent {
+    constructor () {
+    }
+}
